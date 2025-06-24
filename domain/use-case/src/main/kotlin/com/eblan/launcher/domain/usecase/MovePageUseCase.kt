eb01193d33b76dcae54a80d9f23d15ed86a20343
JackEblan <jackeblan115@gmail.com>
2025-06-24 08:55:44 +0800

    Added Move page use case initially not tested yet

---
+++ domain/use-case/src/main/kotlin/com/eblan/launcher/domain/usecase/MovePageUseCase.kt
@@ -0,0 +1,37 @@
+package com.eblan.launcher.domain.usecase
+
+import com.eblan.launcher.domain.model.Associate
+import com.eblan.launcher.domain.repository.GridCacheRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class MovePageUseCase @Inject constructor(
+    private val gridCacheRepository: GridCacheRepository,
+) {
+    suspend operator fun invoke(from: Int, to: Int) {
+        withContext(Dispatchers.Default) {
+            val gridItems = gridCacheRepository.gridCacheItems.first()
+                .filter { it.associate == Associate.Grid }
+
+            val movePageMovingGridItems =
+                gridItems.filter { gridItem -> gridItem.page == from }
+                    .map { gridItem -> gridItem.copy(page = to) }
+
+            val movePageOtherGridItems = gridItems.filter { it.page != from }.map { gridItem ->
+                when {
+                    from < to && gridItem.page in (from + 1)..to ->
+                        gridItem.copy(page = gridItem.page - 1)
+
+                    from > to && gridItem.page in to until from ->
+                        gridItem.copy(page = gridItem.page + 1)
+
+                    else -> gridItem
+                }
+            }
+
+            gridCacheRepository.upsertGridItems(gridItems = movePageMovingGridItems + movePageOtherGridItems)
+        }
+    }
+}

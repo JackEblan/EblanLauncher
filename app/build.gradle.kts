31af51eb0d0dc41f299ae0cc3a937cce90cd85c9
JackEblan <jackeblan115@gmail.com>
2025-09-15 07:07:04 +0800

    Updated application version name
    This commit updates the `versionName` in `app/build.gradle.kts`.
    
    - Changed `versionName` from "1.0" to "0.1.0-alpha1".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.1.0-alpha1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

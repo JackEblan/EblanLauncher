bb9ce0380205efe0e26baa592b6ab67def7b5de9
JackEblan <jackeblan115@gmail.com>
2025-09-25 09:37:38 +0800

    Release: Version 0.1.0-alpha5
    This commit bumps the app version to 0.1.0-alpha5 and updates the changelog.
    
    - **`app/build.gradle.kts`**:
        - `versionCode` updated from 4 to 5.
        - `versionName` updated from "0.1.0-alpha4" to "0.1.0-alpha5".
    - **`fastlane/metadata/android/en-US/changelogs/5.txt`**:
        - Added changelog for version 5:
            - Fixed an unexpected crash that occurred when opening the app
            - Improved drag-and-drop item animations for smoother interactions
            - Improved Shortcut icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.eblan.launcher"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.1.0-alpha4"
+        versionCode = 5
+        versionName = "0.1.0-alpha5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

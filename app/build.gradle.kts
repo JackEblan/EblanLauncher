57564ce27dff519bf4c8c695864f9890be6c00fa
JackEblan <jackeblan115@gmail.com>
2025-09-26 08:53:31 +0800

    Updated version and added changelog
    This commit increments the application version code to 6 and version name to "0.1.0-alpha6". It also introduces a new changelog file for version 6.
    
    - **app/build.gradle.kts:**
        - `versionCode` updated from 5 to 6.
        - `versionName` updated from "0.1.0-alpha5" to "0.1.0-alpha6".
    - **fastlane/metadata/android/en-US/changelogs/6.txt:**
        - New file created with the following changes:
            - Improved activities launch behavior
            - Fixed overlapping grid items at the edges
            - Added App Drawer alphabet scrollbar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.eblan.launcher"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.1.0-alpha5"
+        versionCode = 6
+        versionName = "0.1.0-alpha6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

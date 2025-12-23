34939b075d8477c237a7649eef4c057017bebca1
Jack Eblan <jackeblan115@gmail.com>
2025-12-23 10:44:56 +0800

    Feat: Bump version to 0.3.0-alpha
    This commit increments the application version for the new release.
    
    - The `versionCode` is updated from 29 to 30.
    - The `versionName` is updated from "0.2.9-alpha" to "0.3.0-alpha".
    
    A new changelog file (`30.txt`) has been created to document the key changes in this version, including the new experimental "lock movement" setting, automatic home screen population on first launch, and other fixes and refactors.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
 
     defaultConfig {
         applicationId = "com.eblan.launcher"
-        versionCode = 29
-        versionName = "0.2.9-alpha"
+        versionCode = 30
+        versionName = "0.3.0-alpha"
     }
 
     buildTypes {

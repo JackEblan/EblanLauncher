ef0bb60741717cf919fbbf6ce37902f2e8fa2e71
Jack Eblan <jackeblan115@gmail.com>
2026-01-26 17:49:19 +0800

    Refactor: Update `pointerInput` keys to recompose on swipe state changes
    This commit refactors the `pointerInput` modifier in `InteractiveGridItem` to ensure that drag gesture detection is recomposed when swipe-related properties change.
    
    The keys for the `pointerInput` have been updated from a static `Unit` to `gridItem.swipeUp` and `gridItem.swipeDown`. This change ensures that the gesture detection logic is correctly re-initialized if the swipe actions are enabled or disabled, preventing stale behavior.

--- feature/home/src/main/kotlin/com/eblan/launcher/feature/home/component/grid/InteractiveGridItem.kt
+++ feature/home/src/main/kotlin/com/eblan/launcher/feature/home/component/grid/InteractiveGridItem.kt
@@ -1142,7 +1142,10 @@ private fun Modifier.swipeGestures(
                 GRID_ITEM_MAX_SWIPE_Y.dp.roundToPx()
             }
 
-            pointerInput(key1 = Unit) {
+            pointerInput(
+                key1 = gridItem.swipeUp,
+                key2 = gridItem.swipeDown,
+            ) {
                 detectVerticalDragGestures(
                     onDragStart = {
                         scope.launch {
